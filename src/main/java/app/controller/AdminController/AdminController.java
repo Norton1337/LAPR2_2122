0d8ce15b155a47e81bc4e81e3789feed6fd4f88e
NunoBarbosa20 <1210822@isep.ipp.pt>
2022-05-27 09:36:24 +0100

    T-047|Implemented US14 bug fixes

--- src/main/java/app/controller/AdminController/AdminController.java
+++ src/main/java/app/controller/AdminController/AdminController.java
@@ -19,16 +19,22 @@ public class AdminController {
         this.company=company;
     }
 
-    public List importFromFile(String filename) throws FileNotFoundException {
-        boolean header= true ;
+    public List importFromFile(String filename,int option) throws FileNotFoundException {
+        boolean header = true;
         List<List<String>> records = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
             String line;
             while ((line = br.readLine()) != null) {
-                String[] values = line.split(",");
-                if (header &&  !(values[0].indexOf(0)<= 9 && values[0].indexOf(0) >= 0))
-                    header = false;
-                else records.add(Arrays.asList(values));
+                if (option==1){
+                    String[] values = line.split(";");
+                    if (header &&  !(values[0].indexOf(0)<= 9 && values[0].indexOf(0) >= 0))
+                        header = false;
+                    else records.add(Arrays.asList(values));
+                }
+                else{
+                    String[] values = line.split(",");
+                    records.add(Arrays.asList(values));
+                }
 
             }
         } catch (IOException e) {
@@ -45,8 +51,6 @@ public class AdminController {
         return records;
     }
 
-    public void importFromFileWithHeader(String filename) {
-    }
 
     public void printList() {
         for (SnsUser user:this.company.listSnsUser()) {
